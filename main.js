c52f25fc45bc5cd41091841adbe81ddb9ceb9c92
sgujuluva <sanghee04@gmail.com>
2022-06-08 13:37:00 +0200

    quotes added

--- main.js
+++ main.js
@@ -9,7 +9,8 @@ let array = ["Be yourself; everyone else is already taken.","I'm selfish, impati
 "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
 "You've gotta dance like there's nobody watching, Love like you'll never be hurt,Sing like there's nobody listening,And live like it's heaven on earth.",
 "You know you're in love when you can't fall asleep because reality is finally better than your dreams.",
-"You only live once, but if you do it right, once is enough.","Be the change that you wish to see in the world"];
+"You only live once, but if you do it right, once is enough.","Be the change that you wish to see in the world",
+"In three words I can sum up everything I've learned about life: it goes on."];
 //to create random text from array
 function random(){
   return p.innerHTML = array[Math.floor(Math.random() * array.length)];
